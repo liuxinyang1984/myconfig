0dc73cd09bcfbdcf9f2398f5b1df98e83c9bb2b0
liuxinyang1984 <liuxinyang1984@gmail.com>
2023-08-09 14:26:50 +0800

    更新sshhost

--- .sshhost.sh
+++ .sshhost.sh
@@ -2,4 +2,5 @@ alias sshbjcs='ssh root@112.126.79.220'
 alias sshbjproduct='ssh prod_xxx@182.92.75.214'
 alias sshcs='ssh root@39.99.144.231'
 alias sshjp='ssh root@198.13.32.171'
+alias sshus='ssh root@154.22.235.237 -p 19080'
 

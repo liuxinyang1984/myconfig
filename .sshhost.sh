b7c3b275d376cd821a72e19255528092a686c917
liuxinyang1984 <liuxinyang1984@gmail.com>
2023-09-21 12:33:42 +0800

    update sshhost

--- .sshhost.sh
+++ .sshhost.sh
@@ -1,7 +1,10 @@
 alias sshbjcs='ssh root@112.126.79.220'
 alias sshbjproduct='ssh prod_xxx@182.92.75.214'
 alias sshcs='ssh root@39.99.144.231'
-alias sshjp='ssh root@198.13.32.171'
+alias sshjp='ssh root@64.176.50.95'
 alias sshus="ssh root@154.22.235.237 -p 19080"
 alias sshlove="ssh root@114.115.133.209"
 alias sshsanzhu="ssh root@49.232.77.217"
+alias sshusliu=:"ssh root@64.176.50.95"
+alias sshwtyxb=:"ssh prod_xxx@110.42.231.246"
+alias sshtyy=:"ssh root@182.40.34.27"
